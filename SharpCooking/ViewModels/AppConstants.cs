b46f15b5f41dd9374abdfe4ee481d64cfe285b02
lpains <jlucaspains@gmail.com>
2020-07-12 11:56:22 -0600

    Fixed links.

--- SharpCooking/ViewModels/AppConstants.cs
+++ SharpCooking/ViewModels/AppConstants.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections.Generic;
-
-namespace SharpCooking.ViewModels
+﻿namespace SharpCooking.ViewModels
 {
     public class AppConstants
     {
@@ -9,9 +7,9 @@ namespace SharpCooking.ViewModels
         internal const string BackupIsSetup = "BackupIsSetup";
         internal const string StepTimeIdentifierRegex = "(?<Minutes>\\d+)\\s*(minutes|minute|min)|(?<Hours>\\d+)\\s*(hours|hour)|(?<Days>\\d+)\\s*(days|day)";
         internal const string IngredientQuantityRegex = "(?<Fraction>\\d+\\/\\d+)|(?<Regular>\\d+)";
-        internal const string SupportUri = "https://sharpcooking.net";
+        internal const string SupportUri = "http://sharpcooking.net/documentation";
         internal const string Language = "DisplayLanguage";
-        internal const string PrivacyPolicyUrl = "https://sharpcooking.net/privacypolicy";
+        internal const string PrivacyPolicyUrl = "http://sharpcooking.net/privacypolicy";
         internal const string DropBoxAccessToken = "DropBoxAccessToken";
         internal static string[] BackupFileMimeTypes = new string[] { "application/zip", "com.pkware.zip-archive" };
         internal const string BackupRecipeFileName = "SharpBackup_Recipe.json";
