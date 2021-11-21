834dc39b256a14267d117071a9d1ad85d1c7556c
Lucas Pains <lpains@jbssa.com>
2021-11-20 19:09:00 -0700

    Translations!

--- SharpCooking/Localization/Resources.Designer.cs
+++ SharpCooking/Localization/Resources.Designer.cs
@@ -19,7 +19,7 @@ namespace SharpCooking.Localization {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "17.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     public class Resources {
@@ -1033,7 +1033,7 @@ namespace SharpCooking.Localization {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to Oh no! Your backup lookups corrupted and I wasn&apos;t able to restore it..
+        ///   Looks up a localized string similar to Oh no! Your backup looks corrupted and I wasn&apos;t able to restore it..
         /// </summary>
         public static string SettingsView_CorruptedBackupFile {
             get {
